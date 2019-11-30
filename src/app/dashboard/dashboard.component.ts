2d2de404d5f6901973e48c0e6f6bffdffb41015f
girvain <gavinross88@hotmail.co.uk>
2019-11-30 20:42:17 +0000

    working unfinished version with werid design of form, result and dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,41 +1,36 @@
-import { Component, Input, Output, EventEmitter, ViewChild, OnInit} from '@angular/core';
+import { Component, Input, Output, EventEmitter, ViewChild, OnInit } from '@angular/core';
 import { TalksService } from '../talks.service';
 import { TalkSearchComponent } from '../talk-search/talk-search.component';
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+    selector: 'app-dashboard',
+    templateUrl: './dashboard.component.html',
+    styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-  unfilteredTalks: object[];
-     talks: object[];
+    unfilteredTalks: object[];
+    talks: object[];
     speakers: string[];
-    selectOptions: object = {speakers: []};
-    @ViewChild(TalkSearchComponent, {static: false}) talkSearch: TalkSearchComponent;
+    selectOptions: object = {};
+    @ViewChild(TalkSearchComponent, { static: false }) talkSearch: TalkSearchComponent;
 
 
-  constructor(private talksService: TalksService) { }
+    constructor(private talksService: TalksService) { }
 
-  ngOnInit() {
-    this.getTalks();
-  }
+    ngOnInit() {
+        this.getTalks();
+    }
 
-  getTalks(): void {
-    this.talksService.getTalks().subscribe( result => {
-      this.unfilteredTalks = result;
-        // make a deep copy of result talks
-        this.copyTalks();
-        //this.talks = JSON.parse(JSON.stringify(this.unfilteredTalks));
-        // call the child component function
-        //this.talkSearch.setSpeakers();
-        this.filterBySpeakers();
-    });
-  }
+    getTalks(): void {
+        this.talksService.getTalks().subscribe(result => {
+            this.unfilteredTalks = result;
+            // make a deep copy of result talks
+            this.copyTalks();
+            //this.talks = JSON.parse(JSON.stringify(this.unfilteredTalks));
+            this.filterBySpeakers();
+        });
+    }
 
-    filterTalks(): void{
-        this.talks.map(talk => {talk.speaker = 'Gavin Ross'});
-  }
     filterBySpeakers(): void {
         //this.speakers = this.unfilteredTalks.map(talk => talk = talk.speaker);
         this.selectOptions.speakers = this.unfilteredTalks.map(talk => talk = talk.speaker);
