aa4b9c687388d65dff789ee31eb858ab88d4fba8
girvain <gavinross88@hotmail.co.uk>
2019-12-29 10:03:10 +0000

    Auto-generated commit

---
+++ conference/scripts.2114e7ebed57f4313804.js
@@ -0,0 +1,1 @@
+$(document).ready((function(){$(".ui.dropdown").dropdown(),$(".ui .rating").rating({initialRating:0,maxRating:5}),$(".ui .rating").rating("disable")})),$(".dropdown").dropdown(),$(".ui.sidebar").sidebar("toggle"),$(".context.example .ui.sidebar").sidebar({context:$(".context.example .bottom.segment")}).sidebar("attach events",".context.example .menu .item");
